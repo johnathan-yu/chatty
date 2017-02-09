c5c523d196ef1818c181ffe2d7be99cbec6282ca
Johnathan Yu <jyu@legalzoom.com>
2017-02-08 16:45:48 -0800

    more gulp setup

--- gulp.config.js
+++ gulp.config.js
@@ -5,9 +5,37 @@ module.exports = function () {
     var config = {
         // all js to vet
         alljs: [
-            './src/**/*.js',
-            './*.js'
-        ]
+            '*.js',
+            'src/**/*.js',
+            '!src/lib/**/*.js',
+            '!node_modules/**/*.js'
+        ],
+        
+        allCss: [
+            'src/**/*.css'
+        ],
+        
+        bowerJson: './bower.json',
+        
+        env: {
+            port: 3000,
+            monitorDelay: 1
+        },
+        
+        // Inject
+        customJs: './src/js/*.js',
+        customCss: './src/css/*.css',
+        injectIgnorePath: '/src',
+        wiredepIgnorePath: '../',
+        viewSrc: './src/view',
+        
+        startScript: './src/app.js',
+        
+        clientSrc: './src/',
+        
+        browserReloadDelay: 1,
+        
+        build: './build/'
     };
     
     return config;
--- gulpfile.js
+++ gulpfile.js
@@ -3,31 +3,56 @@
     'use strict';
     var gulp = require('gulp');
     var args = require('yargs').argv;
+    var browserSync = require('browser-sync');
+    var wiredep = require('wiredep').stream;
     var $ = require('gulp-load-plugins')({lazy: true});
     var gulpif = require('gulp-if'); // Cannot lazy load this because of reserve word "if" issue.
     var config = require('./gulp.config')();
-    
-//        jshint = require('gulp-jshint'),
-//        jscs = require('gulp-jscs'),
-//        util = require('gulp-util'),
-//        gulpprint = require('gulp-print'),
-//        gulpif = require('gulp-if')
-        
     
     function log(msg) {
         if (typeof (msg) === 'object') {
             var item;
             for (item in msg) {
                 if (msg.hasOwnProperty(item)) {
-                    $.util.log($.util.colors.blue(msg[item]));
+                    $.util.log($.util.colors.cyan(msg[item]));
                 }
             }
         } else {
-            $.util.log($.util.colors.blue(msg));
+            $.util.log($.util.colors.cyan(msg));
         }
     }
+    
+    function startBrowserSync() {
+        if (args.nosync || browserSync.active) {
+            return;
+        }
+        
+        log('Starting browser-sync on port ' + config.env.port);
+        
+        var options = {
+            proxy: 'localhost:' + config.env.port,
+            port: 4000,
+            files: [config.clientSrc + '**/*.*'],
+            ghostMode: {
+                clicks: true,
+                location: false,
+                forms: true,
+                scroll: true
+            },
+            injectChanges: true,
+            logFileChanges: true,
+            logLevel: 'debug',
+            logPrefix: 'chatty',
+            notify: true,
+            reloadDelay: 1000
+        };
+        browserSync(options);
+    }
 
-    ///////////
+    /////////// Tasks ///////////////
+    
+    gulp.task('help', $.taskListing);
+    gulp.task('default', ['help']);
 
     gulp.task('vet', function () {
         log('Analyzing source with JSHint and JSCS');
@@ -40,6 +65,81 @@
             $.jshint()
         ).pipe(
             $.jshint.reporter('jshint-stylish', {verbose: true})
+        );
+    });
+    
+    gulp.task('wiredep', function () {
+        log('Injecting bower depencies');
+
+        var options = {
+            bowerJson: require(config.bowerJson),
+            ignorePath: config.wiredepIgnorePath
+        };
+        
+        return gulp.src(config.viewSrc + '/*.html').pipe(
+            wiredep(options)
+        ).pipe(
+            gulp.dest(config.viewSrc)
+        );
+    });
+    
+    gulp.task('inject', function () {
+        log('Injecting custom depencies');
+        
+        var customJsCss = [].concat(config.customJs, config.customCss);
+        var injectSrc = gulp.src(customJsCss, {read: false});
+        var injectOptions = {
+            ignorePath: config.injectIgnorePath
+        };
+        
+        return gulp.src(config.viewSrc + '/*.html').pipe(
+            $.inject(injectSrc, injectOptions)
+        ).pipe(
+            gulp.dest(config.viewSrc)
+        );
+    });
+    
+    gulp.task('serve', function () {
+        var options = {
+            script: config.startScript,
+            delayTime: config.monitorDelay,
+            env: {
+                'PORT': config.env.port
+            },
+            watch: config.alljs
+        };
+        
+        return $.nodemon(options).on(
+            'restart',
+            function (event) {
+                // Note that restart will trigger start event.  Don't call
+                // task that start event will call because it's redundant.
+                log('File changed: ' + event);
+                
+                setTimeout(function () {
+                    browserSync.notify('reloading now ...');
+                    browserSync.reload({stream: false});
+                }, config.browserReloadDelay);
+                
+                log('*** nodemon restarted');
+            }
+        ).on(
+            'start',
+            ['wiredep', 'inject', 'vet'],
+            function () {
+                startBrowserSync();
+                log('*** nodemon started');
+            }
+        ).on(
+            'crash',
+            function () {
+                log('*** nodemon crashed');
+            }
+        ).on(
+            'exit',
+            function () {
+                log('*** nodemon exited');
+            }
         );
     });
 }());
